4e75fc1cbfdf78646f274ab14d3ba101435ab717
Cardorel <54881922+Cardorel@users.noreply.github.com>
2022-04-26 02:22:57 +0200

    contact changed

--- src/Components/CONTACT/Left.jsx
+++ src/Components/CONTACT/Left.jsx
@@ -6,9 +6,13 @@ export default function Left(){
       <h3>EMAIL</h3>
       <p>cardorelngassaki@gmail.com</p>
       <h3>PHONE(S)</h3>
-      <p>+38 (063) 460 75 69 | +38 (050) 821 13 46</p>
+      <p>+4915213871936</p>
       <h3>ADRESSE(S)</h3>
-      <p>Ukraine, kiev</p>
+      <p>Miesbach, Germany</p>
+      <h3>Network</h3>
+      <p>
+        <a className="link" href="https://api.whatsapp.com/send?phone=+380634607569">Whatsapp</a>
+      </p>
     </div>
   );
 }
